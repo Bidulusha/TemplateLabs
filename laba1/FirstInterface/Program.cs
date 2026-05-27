d93c6b46c5f0fb2e75767b84da4705d40a909ff9
Andreev Vadim Antonovich <vadya22121@mail.ru>
2026-05-27 22:17:06 +0300

    Add lab1 some files

--- laba1/FirstInterface/Program.cs
+++ laba1/FirstInterface/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+using System.Collections;
+using System.Numerics;
+public class FirstInterface
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car(0);
+
+        int fuel = int.Parse(Console.ReadLine());
+        if (car.Refuel(fuel))
+        {
+            car.Drive();
+        }
+    }
+    public interface IVeicle
+    {
+        void Drive();
+        bool Refuel(int amount);
+    }
+    public class Car : IVeicle
+    {
+        public int Fuel { get; set; }
+
+        public Car(int fuel)
+        {
+            Fuel = fuel;
+        }
+        public void Drive()
+        {
+            if (Fuel > 0)
+            {
+                Console.WriteLine("Driving");
+            }
+            else
+            {
+                Console.WriteLine("Not fuel");
+            }
+        }
+
+        public bool Refuel(int refuel)
+        {
+            Fuel = refuel;
+            return true;
+        }
+
+    }
+}
