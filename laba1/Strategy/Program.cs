0f618891f791340edb384ea5564b8f135f7e3943
Andreev Vadim Antonovich <vadya22121@mail.ru>
2026-05-27 22:02:00 +0300

    Add lab1 some files

---
+++ laba1/Strategy/Program.cs
@@ -0,0 +1,101 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Character king = new King();
+        WeaponBehavior weaponBehavior = new SwordBehavior();
+        king.SetWeapon(weaponBehavior);
+        king.fight();
+
+        WeaponBehavior weaponBowSndArrowBehavior = new BowAndArrowBehavior();
+        king.SetWeapon(weaponBowSndArrowBehavior);
+        king.fight();
+    }
+    public interface WeaponBehavior
+    {
+        void UseWeapon();
+    }
+    public class SwordBehavior : WeaponBehavior
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Attack with sword");
+        }
+
+    }
+    public class KnightBehavior : WeaponBehavior
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Attack with knife");
+        }
+
+    }
+    public class BowBehavior : WeaponBehavior
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Attack with bow");
+        }
+
+    }
+    public class AxeBehavior : WeaponBehavior
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Attack with axe");
+        }
+
+    }
+    public class BowAndArrowBehavior : WeaponBehavior
+    {
+        public void UseWeapon()
+        {
+            Console.WriteLine("Attack with bow and arrow");
+        }
+
+    }
+
+    public abstract class Character
+    {
+        public WeaponBehavior weapon;
+
+        public void SetWeapon(WeaponBehavior w)
+        {
+            weapon = w;
+        }
+        public abstract void fight();
+    }
+    public class King : Character
+    {
+        public override void fight()
+        {
+            Console.WriteLine("King: ");
+            weapon.UseWeapon();
+        }
+    }
+
+    public class Queen : Character
+    {
+        public override void fight()
+        {
+            Console.WriteLine("Queen: ");
+        }
+    }
+
+    public class Knight : Character
+    {
+        public override void fight()
+        {
+            Console.WriteLine("Knight: ");
+        }
+    }
+
+    public class Troll : Character
+    {
+        public override void fight()
+        {
+            Console.WriteLine("Troll: ");
+        }
+    }
+}
---
+++ laba1/StudentAndTeacherTest/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Program
+{
+    public static void  Main()
+    {
+        Person person = new Person();
+        person.greet();
+
+        Student student = new Student();
+        student.greet();
+        student.SetAge(18);
+        student.Study();
+        student.showage();
+
+        Teacher teacher = new Teacher();
+        teacher.greet();
+        teacher.Explane();
+    }
+}
+
+public class Person
+{
+    protected int age;
+    public void greet(){
+        Console.WriteLine("Hello!");
+    }
+    public void SetAge(int n)
+        { age = n; }
+}
+
+class Student : Person
+{
+    public void Study()
+    { Console.WriteLine("I'm studying");
+    }
+    public void showage()
+    {
+        Console.WriteLine("My age id " + age + "years old");
+    }
+}
+
+public class Teacher: Person
+{
+    public void Explane()
+    {
+        Console.WriteLine("I'm explaining");
+    }
+}
